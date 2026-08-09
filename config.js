aac9c9cb38f7212edcb8202e930bdf7f3ea3dc83
wacner <wacnerg@gmail.com>
2026-08-09 18:14:26 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+
+// config.js
+// Reemplazar con la URL real del Apps Script
+const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwh4jBG5GcEZxsGRAU765REkmrEZusBy3xeMOGknh9PZ7YZi3sU3qtZRxm4VgJ92kI2Yw/exec';
