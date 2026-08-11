de027afd09329d9800d744d5fbb123b1b6a513d2
wacner <wacnerg@gmail.com>
2026-08-11 16:50:16 -0400

    Update APP_SCRIPT_URL to new endpoint

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
 // config.js
 // Reemplazar con la URL real del Apps Script
-const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwrPB7QigwUgibJ9fKMAoo5NGQZJCeZl4l06e1_RMU6O-6hBGeg02rAjR_ItDoW7nrO/exec';
+const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyn79aMhb9sZ9PNfYh_KFm1328EGdBHEjjJW0le1KCWoZ1w-kI1Uwh0Fw7jJxpxlI9B/exec';
