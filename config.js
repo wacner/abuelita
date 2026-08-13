7bbdaa7652d8160219fde8233869953468e74c5c
wacner <wacnerg@gmail.com>
2026-08-13 16:44:36 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
 // config.js
 // Reemplazar con la URL real del Apps Script
-const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxShm3TrBplgtT2zxE2isVm3pM8bWRX_6-wBTjSyYnFfPjyDqsqrQVKubxydbdGq_BC/exec';
+const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyYdBfQuO68xv_qWABdju9L1MdPyELi0f1rAvBH8XiXPXeeBM9b9dE0FwY0Yhpqrrcz/exec';
